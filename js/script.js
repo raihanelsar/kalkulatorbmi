cd258ee7c393275508eff00f3e7d13d8cee61efb
raihanelsar <raihan.elsar21@gmail.com>
2024-08-24 00:21:43 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,26 +1,44 @@
-function Calculate() {
-    var gender = document.getElementById('g-input').value;
-    var age = document.getElementById('a-input').value;
-    var height = document.getElementById("h-input").value;
-    var weight = document.getElementById("w-input").value;
-  
-    if (!gender || !age || !height || !weight) {
-        alert("Please Insert All Data!");
-        return;
-    }
-  
-    var result = parseFloat(weight) / (parseFloat(height) / 100) ** 2;
-  
-    if (!isNaN(result)) {
-        document.getElementById("bmi-output").innerHTML = result.toFixed(2); // Membulatkan hasil ke 2 desimal
-        if (result < 18.5) {
-            document.getElementById("bmi-status").innerHTML = "Underweight";
-        } else if (result < 25) {
-            document.getElementById("bmi-status").innerHTML = "Healthy";
-        } else if (result < 30) {
-            document.getElementById("bmi-status").innerHTML = "Overweight";
-        } else {
-            document.getElementById("bmi-status").innerHTML = "Obese";
-        }
-    }
+const bmiText = document.getElementById("bmi");
+const descText = document.getElementById("desc");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", onFormSubmit);
+form.addEventListener("reset", onFormReset);
+
+function onFormReset() {
+  bmiText.textContent = 0;
+  bmiText.className = "";
+  descText.textContent = "N/A";
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const weight = parseFloat(form.weight.value);
+  const height = parseFloat(form.height.value);
+
+  if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+    alert("Please enter a valid weight and height");
+    return;
   }
+
+  const heightInMeters = height / 100; // cm -> m
+  const bmi = weight / Math.pow(heightInMeters, 2);
+  const desc = interpretBMI(bmi);
+
+  bmiText.textContent = bmi.toFixed(2);
+  bmiText.className = desc;
+  descText.innerHTML = `You are <strong>${desc}</strong>`;
+}
+
+function interpretBMI(bmi) {
+  if (bmi < 18.5) {
+    return "underweight";
+  } else if (bmi < 25) {
+    return "healthy";
+  } else if (bmi < 30) {
+    return "overweight";
+  } else {
+    return "obese";
+  }
+}
